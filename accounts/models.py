605e2fcd240a513ec3ff149ff248b6250e7e36ff
Abhijit Barui <abhijitongit@gmail.com>
2025-06-13 16:00:29 +0530

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,40 @@
+# BP for customuser login
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+from django.utils import  timezone
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("email field must be entered")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=True, blank=True)
+
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+
+    full_name = models.CharField(max_length=100)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number', 'full_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email or self.phone_number
+
